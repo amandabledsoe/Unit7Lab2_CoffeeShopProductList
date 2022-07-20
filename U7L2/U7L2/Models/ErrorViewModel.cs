7198583d200ef9ea0853cf2a5c44d523afab9dbd
amandabledsoe <amandajbledsoe@gmail.com>
2022-07-19 21:05:12 -0400

    initial commit with functioning MVC and SQL Database Connected

---
+++ U7L2/U7L2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace U7L2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
